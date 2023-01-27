8afafca5d33473de07f145c4a22d00d4e594db46
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 20:25:14 +0330

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Class1
+    {
+        private int _speed;
+        private string _carname = "elentra";
+        public int Speed
+        {
+            get { return _speed; }
+            set { if (value > 120)
+                {
+                    Console.WriteLine("mimirihaaaaaa....");
+                    _speed = 0;
+                }
+                else
+                    _speed = value;
+            }
+        }
+        public string carname
+        {
+            get { return _carname; }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 pride = new Class1();
+            pride.Speed = 490;
+            Console.WriteLine($"carname is :{pride.carname}");
+            Console.WriteLine ($"pride speed is :{ pride.Speed}");
+            Console.ReadKey();
+        }
+    }
+}
